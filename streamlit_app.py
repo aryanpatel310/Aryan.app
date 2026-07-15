6ce3af5de2603ffb0437464e3c5f65558a0f3567
aryanpatel310 <aryanpatel71581@gmail.com>
2026-07-15 19:38:44 +0000

    new project

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("My name is Aryan")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](."https://github.com/aryanpatel310/Aryan.app")
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://github.com/aryanpatel310/Aryan.app)"
 )
